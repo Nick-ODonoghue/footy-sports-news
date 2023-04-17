3b7b0dbb6f20c88cbb01ca12513c7fa4241a5516
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-17 15:52:41 +0100

    wrote async function returning JSON object to get highlight videos from free-football-soccer-videos API

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// Grab form element from DOM
+const search = document.getElementById("form");
+// console.log(search);
+
+// Add evenListener on form for a submit
+search.addEventListener("submit", (event) => {
+    // Prevent submit page refresh
+    event.preventDefault();
+
+    // Grab users team search
+    const team = event.currentTarget.input.value;
+    console.log(team);
+    getVideos();
+});
+
+async function getVideos() {
+
+    const url = "https://free-football-soccer-videos.p.rapidapi.com/";
+
+    const options = {
+        method: 'get',
+        headers: {
+            'X-RapidAPI-Key': '7243d8fe32msh19c59b5960c320fp1d4c19jsn9810bff971ca',
+            'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com'
+        }
+    };
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(error => console.error('error:' + error));
+}
