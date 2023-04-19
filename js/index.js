42aa761edaa8697aa1eb54c7a0e28bc9fe762f7e
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-19 13:25:13 +0100

    completed getVideos function to grab only the team the user searches for

--- js/index.js
+++ js/index.js
@@ -7,18 +7,20 @@ search.addEventListener("submit", (e) => {
     // Prevent submit page refresh
     e.preventDefault();
 
-    // Grab users input
+    // Grab users team name input
     const team = e.currentTarget.input.value;
-    console.log(team);
-    getVideos();
+
+    // Call getVideos function and pass users team name as argument
+    getVideos(team);
 });
 
-function getVideos() {
+// Function to get back data on users team taking team as a parameter
+function getVideos(team) {
 
     // Setting endpoint to url
     const url = "https://free-football-soccer-videos.p.rapidapi.com/";
 
-    // Creating a new request
+    // Creating a new request instance
     const request = new Request(url, {
 
         method: 'get',
@@ -29,10 +31,18 @@ function getVideos() {
 
     })
 
-    // Passing request to fetchAPI
+    // Passing new request to fetchAPI
     fetch(request)
         .then(response => response.json())
-        .then(response => console.log(response))
+
+        // Itterating over data returned looking for users team
+        .then(data => {
+            data.forEach(game => {
+                if (game.side1.name === team || game.side2.name === team) {
+                    console.log(game)
+                }
+            })
+        })
         .catch(error => console.error('error:' + error));
 
 }
