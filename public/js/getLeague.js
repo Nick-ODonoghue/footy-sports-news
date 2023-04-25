047ab446fb6cc87418ccec69ee15de0ce6027354
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-25 17:42:59 +0100

    added loop in getLeague to return the first league found in the API response

--- public/js/getLeague.js
+++ public/js/getLeague.js
@@ -11,7 +11,21 @@ export const getLeague = async (teamID) => {
     const dataResponse = await response.json()
     console.log(dataResponse)
 
-    const leagueID = dataResponse[0].league.id
+    // Set empty variable to hole the teams league ID
+    let leagueID
+
+    // Loop through the returned data
+    for (let i = 0; i < dataResponse.length; i++) {
+      // Assign each returned object to a variable
+      const obj = dataResponse[i].league
+
+      // Return the league ID of the first object of type 'league'
+      if (obj.type === 'League') {
+        leagueID = obj.id
+        break
+      }
+    }
+
     getStandings(leagueID)
 
   } catch (error) {
--- public/js/getStandings.js
+++ public/js/getStandings.js
@@ -1,7 +1,7 @@
-// Front-end async funtion calling the backend and passing users teamID as a parametor
+// Front-end async funtion calling the backend and passing users team LeagueID as a parametor
 export const getStandings = async (leagueID) => {
   const url = new URL('http://localhost:5555/standings')
-  // Append users team ID that was pulled from getTeams function dataResponse
+  // Append users leagueID that was pulled from getLeague function dataResponse
   url.searchParams.append('id', leagueID)
 
   try {
