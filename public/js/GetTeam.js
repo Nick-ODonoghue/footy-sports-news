1f51dfff4369e05c73d12954e9044d3036681db5
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-25 10:33:37 +0100

    removed axios from the frontend API call, changed to js fetch method

--- public/js/GetTeam.js
+++ public/js/GetTeam.js
@@ -1,21 +1,46 @@
 // Front-end async funtion calling the backend and passing users team as a parametor
+// export const getTeams = async (teamName) => {
+//   const options = {
+//     method: 'GET',
+//     // Function calling our backend URL route defined in our index.js
+//     url: 'http://localhost:5555/team',
+//     // Passing users input as a parametor
+//     params: { name: teamName },
+//   }
+
+//   try {
+//     const response = await axios.request(options)
+//     console.log(response.data)
+//     // Grab the response data
+//     const dataResponse = response.data
+
+//     // Loop through the data
+//     dataResponse.forEach(teams => {
+//       // Grab the teams logo url
+//       const teamLogo = teams.team.logo
+//       // Pass the url to the setImage function
+//       setImage(teamLogo)
+//     })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// Front-end async funtion calling the backend and passing users team as a parametor
 export const getTeams = async (teamName) => {
-  const options = {
-    method: 'GET',
-    // Function calling our backend URL route defined in our index.js
-    url: 'http://localhost:5555/team',
-    // Passing users input as a parametor
-    params: { name: teamName },
-  }
+  // Create a new instance of URL
+  const url = new URL('http://localhost:5555/team')
+  // Append users input to the URL object
+  url.searchParams.append('name', teamName)
 
   try {
-    const response = await axios.request(options)
-    console.log(response.data)
-    // Grab the response data
-    const dataResponse = response.data
+    // Send get request to my backend server
+    const response = await fetch(url.toString())
+    const dataResponse = await response.json()
+    console.log(dataResponse)
 
     // Loop through the data
-    dataResponse.forEach(teams => {
+    dataResponse.forEach((teams) => {
       // Grab the teams logo url
       const teamLogo = teams.team.logo
       // Pass the url to the setImage function
