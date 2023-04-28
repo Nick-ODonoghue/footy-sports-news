53fc8992f03024f6d4d908a943aec57b159a942a
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-28 13:04:00 +0100

    pulling in more team data from getTeams request, & injected into html

--- public/js/getTeam.js
+++ public/js/getTeam.js
@@ -14,14 +14,20 @@ export const getTeams = async (teamName) => {
 
     // Grabbing team ID of users input and passing as an argument into getLeague function
     const teamID = dataResponse[0].team.id
+
     getLeague(teamID)
 
     // Loop through the data
     dataResponse.forEach((teams) => {
       // Grab the teams logo url
       const teamLogo = teams.team.logo
+      const teamCountry = teams.team.country
+      const teamFounded = teams.team.founded
+      const teamStadiumName = teams.venue.name
+      const teamStadiumCapacity = teams.venue.capacity
+
       // Pass the url to the setImage function
-      setImage(teamLogo)
+      setTeamInfo(teamLogo, teamCountry, teamFounded, teamStadiumName, teamStadiumCapacity)
     })
   } catch (error) {
     console.error(error)
@@ -29,14 +35,30 @@ export const getTeams = async (teamName) => {
 }
 
 // Function to inject team logo
-const setImage = (imgUrl) => {
-  // Grab DOM element
-  const logoSection = document.querySelector(".table")
-  // Create new img element
-  const imgElement = document.createElement("img")
+const setTeamInfo = (teamLogo, teamCountry, teamFounded, teamStadiumName, teamStadiumCapacity) => {
+  // Grab DOM elements
+  const logoSection = document.querySelector('.team__logo')
+  const infoSection = document.querySelector('.team__info')
+
+  // Create new elements and set classes
+  const imgEl = document.createElement('img')
+  imgEl.classList.add('team__logo')
+  const countryEl = document.createElement('p')
+  const foundedEl = document.createElement('p')
+  const stadiumNameEl = document.createElement('p')
+  const stadiumCapacityEl = document.createElement('p')
 
   // Set new img elements src to logo URL passed in as an argument
-  imgElement.src = imgUrl
+  imgEl.src = teamLogo
+  countryEl.innerHTML = teamCountry
+  foundedEl.innerHTML = teamFounded
+  stadiumNameEl.innerHTML = teamStadiumName
+  stadiumCapacityEl.innerHTML = teamStadiumCapacity
+
   // Inject logo into html
-  logoSection.appendChild(imgElement)
+  logoSection.appendChild(imgEl)
+  infoSection.appendChild(countryEl)
+  infoSection.appendChild(foundedEl)
+  infoSection.appendChild(stadiumNameEl)
+  infoSection.appendChild(stadiumCapacityEl)
 }
